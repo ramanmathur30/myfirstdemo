24764390ed7b6f1d47845b0b74ed5a8152b3f20d
stellarsoftech <stellarsoftech@gmail.com>
2021-07-30 23:28:40 +0530

    myfirstdemo

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App =()=> {
+
+  // const style={
+  //   color:'blue',fontSize:"15px"
+  // }
+
+  return (
+    <div className="red-color">
+     <h1 style={{color:'blue',fontSize:"15px"}} >Hello World..!!!</h1>
+     <p>hgiueghuiegheugoighioghioghegiohe</p>
+     <p>6515616</p>
+     <p>jfiheueifh</p>
+     <p>334535</p>
+     <img src={logo} alt="" />
+    </div>
+  );
+}
+
+export default App;
